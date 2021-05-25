6cf5e74fab42a4ec45571277a580c65ebed51208
Geewhan Kim <g1kim@ucsd.edu>
2021-05-24 19:50:12 -0700

    Updated to functional login page

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -10,32 +10,67 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // Get elements
-const btnLogin = document.getElementsByClassName('loginbtn')[0];
-const btnSignup = document.getElementsByClassName('signupbtn2')[0];
+const loginForm = document.getElementById('loginform');
+const signupForm = document.getElementById('signupform');
 
 // Add login event
-btnLogin.addEventListener('click', (e) => {
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
     // Get username and password
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     // Sign in
-    firebase.auth().signInWithEmailAndPassword(username, password).catch((e) => {
+    firebase.auth().signInWithEmailAndPassword(username, password).then((e) => {
+        // if correct credentials, goes to main page
+        console.log('Successfully signed in');
+        window.location.href = "index.html";
+    })
+    .catch((e) => {
+        // output error message 
+        const output = document.querySelectorAll('.error')[0];
+        output.textContent = e.message;
         console.log(e.message);
-    });
+    })
+    ;
+
+    loginForm.reset();
 });
 
 // Add signup event
-btnSignup.addEventListener('click', (e) => {
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
     // Get username and password
     const username = document.getElementById('username2').value;
     const password = document.getElementById('password2').value;
-    console.log(username);
 
     // Create account
-    firebase.auth().createUserWithEmailAndPassword(username, password).catch((e) => {
+    firebase.auth().createUserWithEmailAndPassword(username, password).then((e) => {
+        // if account succesfully created, goes to main page
+        console.log('Account successfully created');
+        window.location.href = "index.html";
+    })
+    .catch((e) => {
+        // output error message 
+        const output = document.querySelectorAll('.error')[1];
+        output.textContent = e.message;
         console.log(e.message);
     });
+
+    signupForm.reset();
+});
+
+// ***************************
+// logout for testing purposes
+// ***************************
+const logout = document.getElementsByClassName('logout')[0];
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    firebase.auth().signOut().then(() => {
+        console.log('user signed out');
+    })
 });
 
 // Add a realtime listener
